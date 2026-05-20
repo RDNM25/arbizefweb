60be68c8df10db662a2db6e724639bef3442d661
RDNM <arungswarnabumi@gmail.com>
2026-05-20 07:18:08 +0700

    added sidebar for mobile

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 window.addEventListener('scroll', () => {
     document.getElementById('navbar').classList.toggle('scrolled', window.scrollY > 40);
 });
+
+function showsidebar() {
+    document.getElementById('sidebar').classList.add('open');
+    document.body.style.overflow = 'hidden';
+}
+function hidesidebar() {
+    document.getElementById('sidebar').classList.remove('open');
+    document.body.style.overflow = '';
+}
